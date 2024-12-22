6576b12338d88bf910ff7627530d2e4209c7865a
Carmelo Atienza <atienzabusiness@gmail.com>
2024-12-22 20:58:56 +0800

    feat: prepare sign-in and form component

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
+import { Stack } from 'expo-router'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false
+          }}
+        >
+        </Stack.Screen>
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false
+          }}
+        >
+        </Stack.Screen>
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   )
 }
 
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants'
+import FormField from '@/components/FormField'
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full h-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            style={{ width: 120, height: 35 }}
+          />
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+          <FormField
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
