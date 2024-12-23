bcf1c17425e9614821795be3cb95bc65aea28bac
Carmelo Atienza <atienzabusiness@gmail.com>
2024-12-23 21:42:58 +0800

    chore: refactor fonts import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,21 +4,12 @@ import { useFonts } from 'expo-font';
 import React, { useEffect } from 'react';
 import "../global.css";
 import GlobalProvider from '@/context/GlobalProvider';
+import fonts from '@/constants/fonts';
 
 SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  const [fontsLoaded, error] = useFonts({
-    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
-    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
-    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
-    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-  });
+  const [fontsLoaded, error] = useFonts(fonts);
 
   useEffect(() => {
     if (error) throw error;
