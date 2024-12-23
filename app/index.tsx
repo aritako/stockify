fb040342659fe0d7f5573ff1ba46cdad1eb40c41
Carmelo Atienza <atienzabusiness@gmail.com>
2024-12-23 21:53:37 +0800

    feat(home): redirect when active session

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,18 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
 import { Image, ScrollView, View, Text } from 'react-native';
-import { images } from '@/constants';
-import CustomButton from '@/components/CustomButton';
 import { StatusBar } from 'expo-status-bar';
 import { Redirect, router } from 'expo-router';
 
+import { useGlobalContext } from '@/context/GlobalProvider';
+import CustomButton from '@/components/CustomButton';
+import { images } from '@/constants';
+
 export default function App() {
+  const { isLoading, isLoggedIn } = useGlobalContext();
+  if (!isLoading && isLoggedIn) {
+    return <Redirect href="/home" />;
+  }
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
